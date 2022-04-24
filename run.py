b2e8539397a44e074d9a1af68fa34f37b92741ee
Evaline Wangu <evalinemacharia99@gmail.com>
2022-04-24 15:20:30 +0300

    Add create a function to save user account

--- run.py
+++ run.py
@@ -1,5 +1,14 @@
 # !/usr/bin/env python3.8
 from credentials import Credentials
 from user import User
+# THE user create account##
+def create_useraccount(username, password):
+    new_user = User(username,password)
+    return new_user
 
-def create_useraccount()
+def save_user(user):
+     '''
+    Function to save user account
+    '''
+    contact.save_account()
+
